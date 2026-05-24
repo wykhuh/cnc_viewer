d125f579b78a7bffcc4b47b00bf7efaf0247d42e
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2026-05-23 21:17:41 -0700

    update project csv to exclude projects without observations

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { getAndParseCSV } from "./lib/csv_utils.ts";
 import type { Project, Taxon } from "./types/app";
 
 let projects = (await getAndParseCSV(
-  "/data/cnc_2026_projects.csv",
+  "/data/cnc_2026_projects_with_taxa.csv",
 )) as Project[];
 let taxa = (await getAndParseCSV("/data/cnc_2026_app_taxa.csv")) as Taxon[];
 
